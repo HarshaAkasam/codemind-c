1065cfd5e19c856d7a786f1cd1a813c274542367
HarshaAkasam <119839653+HarshaAkasam@users.noreply.github.com>
2022-12-30 17:46:47 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         int a,b;         scanf("%d%d",&a,&b);         if (a>b)         {             printf("LOSS ");         }         else if(a<b)         {             printf("PROFIT ");         }         else         {             printf("NEUTRAL ");         }     } }

---
+++ Stock_market.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        int a,b;
+        scanf("%d%d",&a,&b);
+        if (a>b)
+        {
+            printf("LOSS
+");
+        }
+        else if(a<b)
+        {
+            printf("PROFIT
+");
+        }
+        else
+        {
+            printf("NEUTRAL
+");
+        }
+    }
+}
