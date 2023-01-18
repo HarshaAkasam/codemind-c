9ee64f1def312b0a2e7af46343b06fb49f1742f0
HarshaAkasam <119839653+HarshaAkasam@users.noreply.github.com>
2023-01-19 00:21:25 +0530

    #include <stdio.h>   int main() {         int data, num1 = 0, num2 = 1, temp, flag = 0;         scanf("%d", &data);
            if (data == num1 || data == num2) {
                    return 0;
            }
            while (num2 <= data) {
                    temp = num2;
                    num2 = num1 + num2;
                    num1 = temp;
                    if (num2 == data) {
                            flag = 1;
                            break;
                    }
            }
            if (flag) {
                    printf("True");
            } else {
                    printf("False");
            }
            return 0;
      }

---
+++ Fibonacci_or_not.c
@@ -0,0 +1,24 @@
+ #include <stdio.h>
+  int main() {
+        int data, num1 = 0, num2 = 1, temp, flag = 0;
+        scanf("%d", &data);
+
+        if (data == num1 || data == num2) {
+                return 0;
+        }
+        while (num2 <= data) {
+                temp = num2;
+                num2 = num1 + num2;
+                num1 = temp;
+                if (num2 == data) {
+                        flag = 1;
+                        break;
+                }
+        }
+        if (flag) {
+                printf("True");
+        } else {
+                printf("False");
+        }
+        return 0;
+  }
